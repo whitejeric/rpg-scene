bc6925701ebc1979e63a751846abd7a70181874e
Eric White <whitejeric@gmail.com>
2023-01-17 18:21:11 -0800

    Delete vite-project directory

--- vite-project/public/models/rename_and_catalogue.py
+++
@@ -1,6 +0,0 @@
-import os
-print (os.getcwd())
-[os.rename(f, f.replace(' ', '_')) for f in os.listdir('.') if not f.startswith('.')]
-for f in os.listdir('.'):
-    if not f.startswith('.'):
-        print(f)
